5eb51e3923ffd8aec0e3482dafdf300971200e78
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-07-14 10:24:36 +0800

    feat: Add collections & products page

---
+++ pages/collections/all.vue
@@ -0,0 +1,163 @@
+<template>
+  <div>
+    <div class="container mx-auto px-4 py-8 md:p-8">
+      <main>
+        <!-- grid -->
+        <div class="grid grid-rows-3 lg:grid-rows-2 lg:grid-cols-12">
+          <!-- Bread Crumbs -->
+          <div class="bread_crumbs lg:row-start-1 lg:row-end-2 lg:col-span-full">
+            <span>
+              <a href="/">首頁</a>
+            </span>
+            >
+            <span>所有商品</span>
+          </div>
+          <!-- title -->
+          <div class="title lg:row-start-2 lg:row-end-3 lg:col-span-full">
+            <h1 class="text-4xl font-semibold text-center mb-3">
+              所有商品
+            </h1>
+          </div>
+          <!-- order -->
+          <div class="sort_by mb-3 flex flex-col lg:row-start-3 lg:row-end-5 lg:col-start-1 lg:col-end-3">
+            <label for="" class="mb-2">排列方式</label>
+            <select id="" name="" class="border border-[#B1B1B1] py-2 pl-4 pr-6 text-sm">
+              <option value="">
+                特色
+              </option>
+              <option value="">
+                按字母順序, A-Z
+              </option>
+              <option value="">
+                按字母順序, Z-A
+              </option>
+              <option value="">
+                價錢, 從低到高
+              </option>
+              <option value="">
+                價錢, 從高到低
+              </option>
+              <option value="">
+                日期, 從新到舊
+              </option>
+              <option value="">
+                日期, 從舊到新
+              </option>
+            </select>
+            <!-- categories -->
+            <br />
+            <div class="categories hidden lg:block lg:row-start-4 lg:row-end-5">
+              <span class="text-xl font-semibold">類別</span>
+              <div class="">
+                <div>
+                  <a href="">
+                    上衣
+                  </a>
+                </div>
+                <div class="leading-7">
+                  <a href="">
+                    People Footwear(加拿大拖鞋品牌)
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- prod_list -->
+          <div class="list_prods grid grid-cols-2 gap-4 lg:grid-cols-4 lg:gap-8 lg:row-start-3 lg:row-end-5 lg:col-start-4 lg:col-end-13">
+            <div class="prod h-full">
+              <div class="prod_img">
+                <img src="https://cdn.store-assets.com/s/274811/i/59927725.jpeg?width=480&format=webp" alt="" class="object-cover">
+              </div>
+              <div class="prod_info p-3 text-center">
+                <div class="prod_info_name text-sm">
+                  ADAM 無葉掛脖風扇 三色
+                </div>
+                <div class="prod_info_price text-sm font-semibold">
+                  NT$ 890
+                </div>
+              </div>
+            </div>
+            <div class="prod h-full">
+              <div class="prod_img">
+                <img src="https://cdn.store-assets.com/s/274811/i/59764434.jpeg?width=480&format=webp" alt="" class="object-cover">
+              </div>
+              <div class="prod_info p-3 text-center">
+                <div class="prod_info_name text-sm break-words">
+                  WODEN X Azurer 2023 Spring & Summer Azurer INCENSE CHAMBER 藍色
+                </div>
+                <div class="prod_info_price text-sm font-semibold">
+                  NT$ 3,380
+                </div>
+              </div>
+            </div>
+            <div class="prod h-full">
+              <div class="prod_img">
+                <img src="https://cdn.store-assets.com/s/274811/i/59927725.jpeg?width=480&format=webp" alt="" class="object-cover">
+              </div>
+              <div class="prod_info p-3 text-center">
+                <div class="prod_info_name text-sm">
+                  ADAM 無葉掛脖風扇 三色
+                </div>
+                <div class="prod_info_price text-sm font-semibold">
+                  NT$ 890
+                </div>
+              </div>
+            </div>
+            <div class="prod h-full">
+              <div class="prod_img">
+                <img src="https://cdn.store-assets.com/s/274811/i/59764434.jpeg?width=480&format=webp" alt="" class="object-cover">
+              </div>
+              <div class="prod_info p-3 text-center">
+                <div class="prod_info_name text-sm">
+                  WODEN X Azurer 2023 Spring & Summer Azurer INCENSE CHAMBER 藍色
+                </div>
+                <div class="prod_info_price text-sm font-semibold">
+                  NT$ 3,380
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const fullPath = route.fullPath
+const params = fullPath.substring(1).split('/')
+const crumbs = []
+
+let path = ''
+
+params.forEach((param) => {
+  path = `${path}/${param}`
+  const match = fullPath.match(path)
+
+  if (match !== null) {
+    crumbs.push(path)
+  }
+})
+
+console.log(crumbs)
+// const crumbs = computed(() => {
+//   const fullPath = route.fullPath
+//   const params = fullPath.substring(1).split('/')
+//   const crumbs = []
+//   let path = ''
+
+//   params.forEach((param, index) => {
+//     path = `${path}/${param}`
+
+//     console.log(path)
+//   })
+//   return crumbs
+// })
+</script>
+
+<style>
+.prod {
+  box-shadow: 0 3px 20px rgba(0,0,0,0.1)
+}
+</style>
---
+++ pages/products/[name].vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="container mx-auto px-4 md:p-8">
+      <main>
+        <div>
+          <!-- Bread Crumbs -->
+          <div class="hidden bread_crumbs lg:block lg:row-start-1 lg:row-end-2 lg:col-span-full">
+            <span class="mr-2">
+              <a href="/">首頁</a>
+            </span>
+            <span class="text-2xl mr-2">
+              ›
+            </span>
+            <span class="mr-2">商品名稱</span>
+          </div>
+          <!-- prod -->
+          <div class="prod">
+            <div class="prod_img ">
+              <img src="https://cdn.store-assets.com/s/274811/i/59927725.jpeg?width=1024&format=webp" alt="" class="max-h-[60vh] mx-auto">
+              <div class="prod_img_list">
+                <ul class="flex flex-wrap">
+                  <li class="w-1/5 p-4">
+                    <img src="https://cdn.store-assets.com/s/274811/i/59927725.jpeg?width=1024&format=webp" alt="" class="max-h-12 w-auto">
+                  </li>
+                  <li class="w-1/5 p-4">
+                    <img src="https://cdn.store-assets.com/s/274811/i/59927641.jpeg?width=1024&format=webp" alt="" class="max-h-12 w-auto">
+                  </li>
+                  <li class="w-1/5 p-4">
+                    <img src="https://cdn.store-assets.com/s/274811/i/59927642.jpeg?width=1024&format=webp" alt="" class="max-h-12 w-auto">
+                  </li>
+                  <li class="w-1/5 p-4">
+                    <img src="https://cdn.store-assets.com/s/274811/i/59927647.jpeg?width=1024&format=webp" alt="" class="max-h-12 w-auto">
+                  </li>
+                  <li class="w-1/5 p-4">
+                    <img src="https://cdn.store-assets.com/s/274811/i/59927647.jpeg?width=1024&format=webp" alt="" class="max-h-12 w-auto">
+                  </li>
+                  <li class="w-1/5 p-4">
+                    <img src="https://cdn.store-assets.com/s/274811/i/59927647.jpeg?width=1024&format=webp" alt="" class="max-h-12 w-auto">
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="prod_info">
+              <h1 class="text-3xl font-medium">
+                商品名稱
+              </h1>
+              <div class="prod_price">
+                <span class="text-xl">NT$ 890</span>
+              </div>
+              <div class="flex text-sm">
+                <label for="" class="w-[23%] ">顏色</label>
+                <div class="style flex">
+                  <input id="color1" type="radio" name="color">
+                  <label for="color1">黑色</label>
+                  <input id="color2" type="radio" name="color">
+                  <label for="color2">沙色</label>
+                  <input id="color3" type="radio" name="color">
+                  <label for="color3">綠色</label>
+                </div>
+              </div>
+              <div class="prod_qty flex">
+                <label class="w-[23%]">數量</label>
+                <a href="#">-</a>
+                <input type="number" value="1" class="text-center">
+                <a href="#">+</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
