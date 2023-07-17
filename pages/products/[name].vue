d2adec27e4ea9c43a3847bcd595d5cc42a8f6b0b
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-07-17 16:34:31 +0800

    feat: get form data in product page

--- pages/products/[name].vue
+++ pages/products/[name].vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="container mx-auto px-4 md:p-8">
+    <div class="max-w-[1400px] mx-auto px-4 md:p-8">
       <main>
         <div>
           <!-- Bread Crumbs -->
@@ -11,11 +11,13 @@
             <span class="text-2xl mr-2">
               ›
             </span>
-            <span class="mr-2">商品名稱</span>
+            <span class="mr-2">
+              {{ name }}
+            </span>
           </div>
           <!-- prod -->
-          <div class="prod">
-            <div class="prod_img ">
+          <div class="prod lg:flex">
+            <div class="prod_img lg:grow">
               <img src="https://cdn.store-assets.com/s/274811/i/59927725.jpeg?width=1024&format=webp" alt="" class="max-h-[60vh] mx-auto">
               <div class="prod_img_list">
                 <ul class="flex flex-wrap">
@@ -41,29 +43,49 @@
               </div>
             </div>
             <div class="prod_info">
-              <h1 class="text-3xl font-medium">
-                商品名稱
-              </h1>
-              <div class="prod_price">
-                <span class="text-xl">NT$ 890</span>
-              </div>
-              <div class="flex text-sm">
-                <label for="" class="w-[23%] ">顏色</label>
-                <div class="style flex">
-                  <input id="color1" type="radio" name="color">
-                  <label for="color1">黑色</label>
-                  <input id="color2" type="radio" name="color">
-                  <label for="color2">沙色</label>
-                  <input id="color3" type="radio" name="color">
-                  <label for="color3">綠色</label>
+              <form id="addToCartForm" action="" @submit.prevent.stop="handleSubmit">
+                <h1 class="text-3xl font-medium">
+                  {{ name }}
+                </h1>
+                <!-- 商品名稱 -->
+                <input type="text" :value="name" name="prodName" class="hidden">
+                <div class="prod_price">
+                  <!-- 商品價格 -->
+                  <input type="number" value="890" name="prodPrice" class="hidden">
+                  <span class="text-xl font-bold">NT$ 890</span>
                 </div>
-              </div>
-              <div class="prod_qty flex">
-                <label class="w-[23%]">數量</label>
-                <a href="#">-</a>
-                <input type="number" value="1" class="text-center">
-                <a href="#">+</a>
-              </div>
+                <div class="flex text-sm">
+                  <!-- 商品顏色 -->
+                  <label for="" class="w-[23%] ">顏色</label>
+                  <div class="style flex">
+                    <input id="color1" type="radio" name="prodColor" value="color1" checked>
+                    <label for="color1">黑色</label>
+                    <input id="color2" type="radio" name="prodColor" value="color2">
+                    <label for="color2">沙色</label>
+                    <input id="color3" type="radio" name="prodColor" value="color3">
+                    <label for="color3">綠色</label>
+                  </div>
+                </div>
+                <div class="prod_qty flex">
+                  <!-- 商品數量 -->
+                  <label class="w-[23%]">數量</label>
+                  <a href="#" @click.stop.prevent="reduceQty">-</a>
+                  <input v-model="prodQty" type="number" class="text-center" name="prodQty">
+                  <a href="#" @click.stop.prevent="addQty">+</a>
+                </div>
+                <div class="btns">
+                  <div>
+                    <a href="">
+                      立即購買
+                    </a>
+                  </div>
+                  <div>
+                    <button type="submit">
+                      加入購物車
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
@@ -71,3 +93,25 @@
     </div>
   </div>
 </template>
+
+<script setup>
+const route = useRoute()
+const { name } = route.params
+const prodQty = ref(1)
+
+const addQty = () => {
+  prodQty.value++
+}
+
+const reduceQty = () => {
+  prodQty.value--
+}
+
+const handleSubmit = (e) => {
+  const form = e.target
+  const formData = new FormData(form)
+  for (const [name, value] of formData.entries()) {
+    console.log(name + ': ' + value)
+  }
+}
+</script>
